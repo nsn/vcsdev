19ff17ea6a704c3a0f0fa39ab3390f49e1385568
Michael Bayer <nsn@nightspawn.com>
2017-01-27 13:25:31 +0100

    bug is in

---
+++ stella.asm
@@ -0,0 +1,238 @@
+; blank vcs 2600 project
+
+    processor 6502
+
+    include "vcs.h"
+    include "macro.h"
+
+;----------------------------
+; Constants
+;----------------------------
+
+; ntsc constants
+VBLANK_WAIT = 41
+NUM_SCANLINES = 191
+
+;--- end Constants
+
+;----------------------------
+; Macros   
+;----------------------------
+
+;############################
+; Bank1
+;############################
+
+;----------------------------
+; Variables
+;----------------------------
+    SEG.U variables
+    ORG $80
+
+tmp                 ds 2
+scanline            ds 1    ; current scanline
+Sec0_PF_l_ptr       ds 2
+Sec0_PF_r_ptr       ds 2
+Sec1_PF_l_ptr       ds 2
+Sec1_PF_r_ptr       ds 2
+
+
+;--- end Variables 
+
+    SEG code
+    ORG $F000
+Reset:
+;----------------------------
+; Start of program
+;----------------------------
+
+; clear RAM
+    sei         ; disable interrupts
+    cld         ; Clear BCD math flag
+    ; set stack pointer to top of RAM
+    ldx #$FF
+    txs
+
+    lda #0
+ClearRam:
+    sta 0,x
+    dex
+    bne ClearRam
+
+; set TIA behaviour
+    ; set bg color to black ($0)
+    lda #$00
+    sta COLUBK
+    ; set pf color to white
+    lda #$0E
+    sta COLUPF
+    ; set pf behaviour
+    lda #%00000000
+    sta CTRLPF
+
+;----------------------------
+; Main Loop
+;----------------------------
+MainLoop:
+    jsr VerticalBlank
+    jsr GameState
+    jsr DrawScreen
+    jsr OverScan
+    jmp MainLoop ; loop forever
+
+;----------------------------
+; vertical blank init
+;----------------------------
+VerticalBlank:
+    ldx #0      
+    lda #2
+    sta WSYNC   ; ?
+    ; begin vertical sync
+    sta VSYNC
+    ; first two lines of vsync
+    sta WSYNC   
+    sta WSYNC   
+    ; use duration 3rd line of VSYNC 
+    ; to set the vertical blank timer
+    lda #VBLANK_WAIT
+    sta TIM64T
+
+    lda #0
+    ; end vsync period
+    sta WSYNC 
+    sta VSYNC
+    rts ;--- VerticalBlank
+
+;----------------------------
+; calculate game state for this frame
+;----------------------------
+GameState:
+
+    rts ;--- GameState
+
+;----------------------------
+; Draw visible scanlines
+;----------------------------
+DrawScreen:
+    ; wait until vertical blank period is over
+    lda INTIM
+    bne DrawScreen
+    sta WSYNC
+    sta VBLANK ; since A = #0
+
+
+    ; Y will be our scanline counter
+    ; ---
+    ; 16 Scanlines of Section0
+    ldy #15
+Section0:
+    sta WSYNC
+    lda (Sec0_PF_l_ptr),y   ; +5
+    and #%1111000           ; +2
+    sta PF0                 ; +3 
+    lda #0                  ; +2    
+    sta PF1                 ; +3 
+    sta PF2                 ; +3 (18) 
+
+    ; wait 10 cycles
+    SLEEP 16
+    lda #0                  ;    
+    sta PF0                 ; +3 (8)
+    sta PF1                 ; +3 (8)
+    lda (Sec0_PF_r_ptr),y
+    and #%11110000
+    sta PF2
+
+    SLEEP 10 
+    ;lda (PF3_data_ptr),y
+    ;sta PF0
+    dey
+    bne Section0
+    ; ---
+    ; 16 Scanlines of Section0
+    ldy #15
+Section1:
+    sta WSYNC
+    lda #%1111000
+    sta PF0
+
+    lda (Sec1_PF_l_ptr),y   ; +5
+    and #%1111000           ; +2
+    sta PF1                 ; +3 
+    lda #0                  ; +2    
+    sta PF2                 ; +3 (18) 
+
+    ; wait 10 cycles
+    SLEEP 16
+
+    lda #0                  ;    
+    sta PF0                 ; +3 (8)
+    lda (Sec1_PF_r_ptr),y
+    sta PF1                 ; +3 (8)
+    and #%11110000
+    sta PF2
+
+    SLEEP 10 
+    ;lda (PF3_data_ptr),y
+    ;sta PF0
+    dey
+    bne Section1
+
+    ldy #191-32
+ScanLoop:
+    lda #0
+    sta PF0
+    sta PF1
+    sta PF2
+    ; WSYNC is placed BEFORE calculations
+    sta WSYNC
+
+    ; work done, dec scanline counter, clean up
+    DEY
+    BNE ScanLoop    
+
+    ; clear registers to prevent bleeding
+    lda #2
+    sta WSYNC   ; finish scanline
+    sta VBLANK  ; make TIA output blank
+    ; re-use Y which is still 0
+    sty PF0
+    sty PF1
+    sty PF2  
+    sty GRP0
+    sty GRP1
+    sty ENAM0
+    sty ENAM1
+    sty ENABL
+    rts ; DrawScreen
+
+;----------------------------
+; Overscan
+;----------------------------
+OverScan:
+    ; wait 30 scanline
+    ldx #30
+OverScanLineWait:
+    sta WSYNC
+    dex
+    bne OverScanLineWait
+    ; return
+    rts
+
+
+;----------------------------
+; Data
+;----------------------------
+
+
+
+;----------------------------
+; Reset/Break 
+;----------------------------
+    ORG $FFFC
+    ; set Reset pointer (at $FFFC and $FFFD) to Reset label 
+    .word Reset
+    ; set BRK pointer (at $FFFE and $FFFF) to Reset label
+    .word Reset
+
+
