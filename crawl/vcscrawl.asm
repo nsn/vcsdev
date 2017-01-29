165b0579bb9b96076907296982ea07507201116c
Michael Bayer <nsn@nightspawn.com>
2017-01-29 17:16:41 +0100

    pre-move

--- crawl/vcscrawl.asm
+++ crawl/vcscrawl.asm
@@ -10,10 +10,14 @@
 ;----------------------------
 
 ; ntsc constants
-VBLANK_WAIT = #43
+VBLANK_WAIT = 42
 NUM_SCANLINES = 191
 
 ;--- end Constants
+
+;----------------------------
+; Macros   
+;----------------------------
 
 ;############################
 ; Bank1
@@ -26,13 +30,10 @@ NUM_SCANLINES = 191
     ORG $80
 
 tmp                 ds 2
-scanline            ds 1    ; current scanline
-PF0_data_ptr        ds 1
-PF1_data_ptr        ds 1
-PF2_data_ptr        ds 1
-PF3_data_ptr        ds 1
-PF4_data_ptr        ds 1
-PF5_data_ptr        ds 1
+Sec0_PF_l_ptr       ds 2
+Sec0_PF_r_ptr       ds 2
+Sec1_PF_l_ptr       ds 2
+Sec1_PF_r_ptr       ds 2
 
 
 ;--- end Variables 
@@ -73,18 +74,17 @@ ClearRam:
 ;----------------------------
 MainLoop:
     jsr VerticalBlank
+    jsr GameState
     jsr DrawScreen
     jsr OverScan
     jmp MainLoop ; loop forever
 
 ;----------------------------
-; vertical blank wait
+; vertical blank init
 ;----------------------------
 VerticalBlank:
-    ldx #0      ; ?
+    ldx #0      
     lda #2
-    sta WSYNC   ; ?
-    sta WSYNC   ; ?
     sta WSYNC   ; ?
     ; begin vertical sync
     sta VSYNC
@@ -93,100 +93,106 @@ VerticalBlank:
     sta WSYNC   
     ; use duration 3rd line of VSYNC 
     ; to set the vertical blank timer
-    lda VBLANK_WAIT
+    lda #VBLANK_WAIT
     sta TIM64T
-    ; clear collision latches
+
     lda #0
-    sta CXCLR
     ; end vsync period
     sta WSYNC 
     sta VSYNC
-    ;----------------
-    ; free cycles!
-    ;----------------
+    rts ;--- VerticalBlank
 
+;----------------------------
+; calculate game state for this frame
+;----------------------------
+GameState:
     ; set playfield data pointers 
     ; according to position in maze
-    lda #>PFData_0_W        ; +2
-    sta PF0_data_ptr+1      ; +3
-    lda #<PFData_0_W        ; +2
-    sta PF0_data_ptr        ; +3 (10)
+    SET_POINTER Sec0_PF_l_ptr, PF_1_0 
+    SET_POINTER Sec0_PF_r_ptr, PF_1_1
 
-    lda #>PFData_1_W        ; +2
-    sta PF1_data_ptr+1      ; +3
-    lda #<PFData_1_W        ; +2
-    sta PF1_data_ptr        ; +3 (10)
+    SET_POINTER Sec1_PF_l_ptr, PF_1_1 
+    SET_POINTER Sec1_PF_r_ptr, PF_1_0
 
-    lda #>PFData_2_W        ; +2
-    sta PF2_data_ptr+1      ; +3
-    lda #<PFData_2_W        ; +2
-    sta PF2_data_ptr        ; +3 (10)
-
-    lda #>PFData_3_W        ; +2
-    sta PF3_data_ptr+1      ; +3
-    lda #<PFData_3_W        ; +2
-    sta PF3_data_ptr        ; +3 (10)
-
-    lda #>PFData_4_W        ; +2
-    sta PF4_data_ptr+1      ; +3
-    lda #<PFData_4_W        ; +2
-    sta PF4_data_ptr        ; +3 (10)
-
-    lda #>PFData_5_W        ; +2
-    sta PF5_data_ptr+1      ; +3
-    lda #<PFData_5_W        ; +2
-    sta PF5_data_ptr        ; +3 (10)
-    ; insert per-frame initializaton code here
-
-
-
-    ; wait until vertical blank period is over
-VerticalBlankWait:
-    lda INTIM
-    bne VerticalBlankWait
-    rts         ; return
+    rts ;--- GameState
 
 ;----------------------------
 ; Draw visible scanlines
 ;----------------------------
 DrawScreen:
-    ; wait for last line to finish
+    ; wait until vertical blank period is over
+    lda INTIM
+    bne DrawScreen
     sta WSYNC
     sta VBLANK ; since A = #0
 
 
     ; Y will be our scanline counter
-    ldy #NUM_SCANLINES
+    ; ---
+    ; 16 Scanlines of Section0
+    ldy #15
+Section0:
+    sta WSYNC
+BREAK:
+    lda (Sec0_PF_l_ptr),y   ; +5
+    and #%11110000          ; +2
+    sta PF0                 ; +3 
+    lda #0                  ; +2    
+    sta PF1                 ; +3 
+    sta PF2                 ; +3 (18) 
+    ; wait for PF0 to finish drawing
+    SLEEP 16
+    lda #0                  ;    
+    sta PF0                 ; +3 (8)
+    sta PF1                 ; +3 (8)
+    ; wait for P23 to finish drawing
+    SLEEP 10
+    lda (Sec0_PF_r_ptr),y
+    and #%11110000
+    sta PF2
+
+    dey
+    bne Section0
+    ; ---
+    ; 16 Scanlines of Section0
+    ldy #15
+Section1:
+    sta WSYNC
+    lda #%11110000
+    sta PF0
+
+    lda (Sec1_PF_l_ptr),y   ; +5
+    and #%11110000          ; +2
+    sta PF1                 ; +3 
+    lda #0                  ; +2    
+    sta PF2                 ; +3 (18) 
+
+    ; wait 10 cycles
+    SLEEP 16
+
+    lda #0                  ;    
+    sta PF0                 ; +3 (8)
+    lda (Sec1_PF_r_ptr),y
+    sta PF1                 ; +3 (8)
+    and #%11110000
+    sta PF2
+
+    SLEEP 10 
+    ;lda (PF3_data_ptr),y
+    ;sta PF0
+    dey
+    bne Section1
+
+    ldy #191-32
 ScanLoop:
+    lda #0
+    sta PF0
+    sta PF1
+    sta PF2
     ; WSYNC is placed BEFORE calculations
     sta WSYNC
-    ; save scanline counter
-    sty scanline            ; +3
-
-    ; block no = y / 4
-    tya                     ; +2
-    lsr                     ; +2
-    lsr                     ; +2
-    ; transfer block_no to y
-    tay                     ; +2 (11)
-
-    ; section no = y/16
-    lsr                     ; +2
-    lsr                     ; +2
-    ; section_idx to x
-    tax                     ; +2 (17)
-
-    ; jump to proper kernel subroutine
-    lda SectionPointersHI,x ; +4
-    pha                     ; +3       
-    lda SectionPointersLO,x ; +4
-    pha                     ; +3
-    rts                     ; +6 (37)
-
-KernelEnd:
 
     ; work done, dec scanline counter, clean up
-    ldy scanline
     DEY
     BNE ScanLoop    
 
@@ -218,76 +224,48 @@ OverScanLineWait:
     ; return
     rts
 
-;----------------------------
-; Kernel(s)
-;----------------------------
-; each kernel is responsible for (at least one) 'section'
-; of the screen, a section most of the time consisting of
-; 4 blocks of 4 scanlines each
-;
-; Kernels expect block_no in y and section_no in x
-;
-; section | scanlines | desc
-;    11   | 191-176   | top of tunnel, nearest
-;    10   |           |
-;     9   |           |
-;     8   |           |
-;     7   |           |
-;     6   |           |
-;     5   |           |
-;     4   |           |
-;     3   |           |
-;     2   |           |
-;     1   |           |
-;     0   |           |
-Section0:
-    lda (PF0_data_ptr),y
-    sta PF0
-    lda (PF3_data_ptr),y
-    sta PF0
-    jmp KernelEnd
-
-; dummy section, just nops
-SectionNOP:
-    nop
-    jmp KernelEnd
-
 
 ;----------------------------
 ; Data
 ;----------------------------
 
-SectionPointersLO:
-    .byte <(SectionNOP)  ; index  0
-    .byte <SectionNOP  ; index  2
-    .byte <SectionNOP  ; index  4
-    .byte <SectionNOP  ; index  6
-    .byte <SectionNOP  ; index  8
-    .byte <SectionNOP  ; index 10
-    .byte <SectionNOP  ; index 12
-    .byte <SectionNOP  ; index 14
-    .byte <SectionNOP  ; index 16
-    .byte <SectionNOP  ; index 18
-    .byte <SectionNOP  ; index 20
-    .byte <(Section0-1)    ; index 22
-
-SectionPointersHI:
-    .byte >(SectionNOP)  ; index  0
-    .byte >SectionNOP  ; index  2
-    .byte >SectionNOP  ; index  4
-    .byte >SectionNOP  ; index  6
-    .byte >SectionNOP  ; index  8
-    .byte >SectionNOP  ; index 10
-    .byte >SectionNOP  ; index 12
-    .byte >SectionNOP  ; index 14
-    .byte >SectionNOP  ; index 16
-    .byte >SectionNOP  ; index 18
-    .byte >SectionNOP  ; index 20
-    .byte >(Section0-1)    ; index 22
-
     include "pfdata.inc"
 
+HI_W_I:
+    .byte #%11110000    
+    .byte #%11110000    
+    .byte #%11110000    
+    .byte #%11110000    
+    .byte #%01110000    
+    .byte #%01110000    
+    .byte #%01110000    
+    .byte #%01110000    
+    .byte #%00110000    
+    .byte #%00110000    
+    .byte #%00110000    
+    .byte #%00110000    
+    .byte #%00010000    
+    .byte #%00010000    
+    .byte #%00010000    
+    .byte #%00010000    
 
+HI_W_N:
+    .byte #%11110000    
+    .byte #%11110000    
+    .byte #%11110000    
+    .byte #%11110000    
+    .byte #%11100000    
+    .byte #%11100000    
+    .byte #%11100000    
+    .byte #%11100000    
+    .byte #%11000000    
+    .byte #%11000000    
+    .byte #%11000000    
+    .byte #%11000000    
+    .byte #%10000000    
+    .byte #%10000000    
+    .byte #%10000000    
+    .byte #%10000000    
 
 
 ;----------------------------
